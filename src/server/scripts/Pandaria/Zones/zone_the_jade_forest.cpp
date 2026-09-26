19d87929f1f817dc3095d0dc419b85a0a39604a5
eddydjcuba <149638238+eddydjcuba@users.noreply.github.com>
2026-09-26 08:23:55 -0400

    Align Jade Forest rope spell hook with DBC effect

--- src/server/scripts/Pandaria/Zones/zone_the_jade_forest.cpp
+++ src/server/scripts/Pandaria/Zones/zone_the_jade_forest.cpp
@@ -5051,12 +5051,6 @@ class spell_jade_forest_rappelling_rope : public SpellScript
         }
     }
 
-    void HandleSummon(SpellEffIndex effIndex)
-    {
-        PreventHitDefaultEffect(effIndex);
-        SummonRope(effIndex);
-    }
-
     void HandleTeleport(SpellEffIndex effIndex)
     {
         // In this 5.4.8 DBC build 130960 is exposed as a teleport effect.
@@ -5075,7 +5069,6 @@ class spell_jade_forest_rappelling_rope : public SpellScript
 
     void Register() override
     {
-        OnEffectLaunch += SpellEffectFn(spell_jade_forest_rappelling_rope::HandleSummon, EFFECT_0, SPELL_EFFECT_SUMMON);
         OnEffectHitTarget += SpellEffectFn(spell_jade_forest_rappelling_rope::HandleTeleport, EFFECT_0, SPELL_EFFECT_TELEPORT_UNITS);
         OnEffectHitTarget += SpellEffectFn(spell_jade_forest_rappelling_rope::HandleInstantLanding, EFFECT_2, SPELL_EFFECT_TRIGGER_SPELL);
     }
